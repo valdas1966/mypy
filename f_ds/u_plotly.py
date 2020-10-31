545d3cb09625dca764de2ac2d31bedd798b084b9
faiterbe <43270828+faiterbe@users.noreply.github.com>
2020-10-31 15:44:00 +0200

    Sqlite

--- f_ds/u_plotly.py
+++ f_ds/u_plotly.py
@@ -17,3 +17,40 @@ def bar(df, col_x, col_y, title):
     fig = px.bar(df, x=col_x, y=col_y, hover_data=[col_x, col_y], color=col_y,
                  height=400, title=title)
     fig.show()
+
+
+def scatter(df, col_x, col_y, col_color, col_size, title):
+    """
+    ============================================================================
+     Description: Plot Interactive Scatter
+    ============================================================================
+     Arguments:
+    ----------------------------------------------------------------------------
+        1. df : DataFrame.
+        2. col_x : str (Column Name of X-Axis).
+        3. col_y : str (Column Name for Y-Axis).
+        4. col_color : str (Column Name to decide the bubble color).
+        5. col_size : str (Column Name to decide the bubble size).
+        6. title : str (Title of the Bar).
+    ============================================================================
+    """
+    fig = px.scatter(df, x=col_x, y=col_y, color=col_color, size=col_size,
+                     hover_data=[col_size], title=title)
+    fig.show()
+
+
+def pie(df, col_name, col_val, title):
+    """
+    ============================================================================
+     Description: Plot Interactive Pie.
+    ============================================================================
+     Arguments:
+    ----------------------------------------------------------------------------
+        1. df : DataFrame
+        2. col_name : str
+        3. col_val : str
+        4. title : str
+    ============================================================================
+    """
+    fig = px.sunburst(df, path=[col_name], values=col_val, title=title)
+    fig.show()
