8600ce0fc771417dcaa9e53165b884b2cbde011b
valdasivanauskas <valdas.ivanauskas.1966@gmail.com>
2021-02-14 14:26:32 +0200

    dg

--- f_utils/u_bin.py
+++ f_utils/u_bin.py
@@ -1,4 +1,8 @@
 
+def text_to_bin(path_text, path_bin):
+
+
+"""
 path_1 = 'G:\\Temp\\1.txt'
 path_2 = 'G:\\Temp\\2.bin'
 path_3 = 'G:\\Temp\\3.txt'
@@ -12,3 +16,4 @@ b = bytearray(txt.encode('utf-8'))
 print(b.hex())
 
 print(bytearray.fromhex(b.hex()).decode())
+"""
