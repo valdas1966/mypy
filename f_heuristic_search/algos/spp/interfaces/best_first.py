a60a84de80682cf654681de07f536b03368b2110
valdasivanauskas <valdas.ivanauskas.1966@gmail.com>
2023-07-20 06:11:35 +0300

    a

--- f_heuristic_search/algos/spp/interfaces/best_first.py
+++ f_heuristic_search/algos/spp/interfaces/best_first.py
@@ -1,19 +1,21 @@
 from f_heuristic_search.algos.spp.interfaces.sppable import SPPAble
+from f_heuristic_search.algos.spp.interfaces.open_closed import OpenClosed
 from f_heuristic_search.problem_types.spp import SPP
 from f_data_structure.cell import Cell
 
 
-class Informed(SPPAble):
+class BestFirst(SPPAble, OpenClosed):
 
     def __init__(self, spp: SPP, to_node: 'NodeClass') -> None:
-        super().__init__(spp=spp)
+        SPPAble.__init__(self, spp=spp)
+        OpenClosed.__init__(self)
         self._to_node = to_node
 
     def search(self) -> None:
         self._generate_node(cell=self.start)
         while self.open:
-            best = self.open.heappop()
-            print(best)
+            best = self.open.pop()
+
 
     def _generate_node(self,
                        cell: Cell,
@@ -25,7 +27,8 @@ class Informed(SPPAble):
         for cell in node.neighbors():
             if cell in self.closed:
                 continue
-            if cell in self.open:
+            elif cell in self.open:
                 if cell.g > node.g + 1:
                     cell.update_parent(node)
-            self._generate_node(cell=cell, parent=node)
+            else:
+                self._generate_node(cell=cell, parent=node)
---
+++ f_heuristic_search/nodes/node.py
@@ -0,0 +1,46 @@
+from f_data_structure.cell import Cell
+
+
+class Node(Cell):
+    """
+    ============================================================================
+     Desc: Represents a Node in the Grid.
+    ============================================================================
+     Inherited Members:
+    ----------------------------------------------------------------------------
+           1. name (str)            : Cell's Name.
+        2. x (int)               : Cell's X-Coordinate.
+        3. y (int)               : Cells' Y-Coordinate.
+        4. is_valid (bool)       : Cell's Validity (Traversability).
+    """
+
+    def __init__(self,
+                 x: int = None,         # Node's X-Coordinate in the Grid.
+                 y: int = None,         # Node's Y-Coordinate in the Grid.
+                 name: str = None,      # Node's Name.
+                 cell: Cell = None,     # Node's Position in the Grid
+                 parent: 'Node' = None  # Parent-Node in the Grid.
+                                        #  None if this is a Start-Node.
+                 ) -> None:
+        """
+        ========================================================================
+         Desc: Init the Node object with Node's Parent and Weight value.
+        ========================================================================
+        """
+        if cell:
+            x, y, name = cell.x, cell.y, cell.name
+        super().__init__(x, y, name)
+        self._w = 1
+        self._parent = parent
+
+    @property
+    def w(self) -> int:
+        return self._w
+
+    @property
+    def parent(self) -> 'Node':
+        return self._parent
+
+    @parent.setter
+    def parent(self, parent_new: 'Node') -> None:
+        self._parent = parent_new
