d8e8ec818f86a81c05ec3b0dcd28a0b616d246d9
valdas1966 <valdas.ivanauskas.1966@gmail.com>
2024-11-14 20:14:46 +0200

    h

--- f_cs/algo.py
+++ f_cs/algo.py
@@ -9,12 +9,22 @@ Ops = TypeVar('Ops')
 
 class Algorithm(Generic[Input, Output, Data, Ops],
                 ProcessIO[Input, Output]):
+    """
+    ============================================================================
+     Abstract-Class for Algorithms in Computer-Science.
+    ============================================================================
+    """
 
     def __init__(self,
                  input: Input,
                  data: Data,
                  ops: Ops,
                  name: str = 'Algorithm') -> None:
+        """
+        ========================================================================
+         Init private Attributes.
+        ========================================================================
+        """
         ProcessIO.__init__(self, input=input, name=name)
         self._data = data
         self._ops = ops
---
+++ f_graph/path_finding/algo.py
@@ -0,0 +1,49 @@
+from f_cs.algo import Algorithm
+from f_graph.path_finding.protocols.problem_path import ProtocolProblemPath
+from f_graph.path_finding.protocols.data import ProtocolData
+from f_graph.path_finding.protocols.ops import ProtocolOps
+from f_graph.nodes.i_1_path import NodePath
+from f_graph.path_finding.path import PathBasic
+from typing import Generic, TypeVar
+
+Problem = TypeVar('Problem', bound=Problem)
+Node = TypeVar('Node', bound=NodePath)
+Path = TypeVar('Path', bound=PathBasic)
+Data = TypeVar('Data', bound=DataPath)
+Ops = TypeVar('Ops', bound=OpsPath)
+
+
+class AlgoPath(Generic[Problem, Path, Data, Ops],
+               Algorithm[Problem, Path, Data, Ops]):
+    """
+    ============================================================================
+     Base-Class for Path-Algorithms.
+    ============================================================================
+    """
+
+    def __init__(self,
+                 problem: Problem,
+                 data: Data,
+                 ops: Ops,
+                 name: str = 'Path-Algorithm') -> None:
+        """
+        ========================================================================
+         Init private Attributes.
+        ========================================================================
+        """
+        Algorithm.__init__(self,
+                           input=problem,
+                           data=data,
+                           ops=ops,
+                           name=name)
+
+    def run(self) -> Path:
+        self._ops.generate(node=self._input.start)
+        while not self._data.has_generated():
+            best = self._data.pop_best_generated()
+            if self._data.is_active_goal(node=best):
+                self._data.remove_goal_active(goal=best)
+                if not self._data.has_active_goals():
+                    self._is_valid = True
+                    return PathBasic()
+                self._ops.explore(node=best)
