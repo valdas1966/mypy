eb2f014397d2c1610d4414022945ecc369a61bb0
valdasivanauskas <valdas.ivanauskas.1966@gmail.com>
2025-01-22 10:33:30 +0200

    h

---
+++ f_graph/path/one_to_one/generators/g_solution.py
@@ -0,0 +1,25 @@
+from f_graph.path.one_to_one.solution import SolutionOneToOne as Solution, StatsPath as Stats
+from f_graph.path.one_to_one.generators.g_problem import GenProblemOneToOne
+from f_graph.path.one_to_one.generators.g_state import GenStateOneToOne
+from f_graph.path.one_to_one.generators.g_cache import GenCache
+from f_graph.path.generators.g_stats import GenStatsPath
+
+
+class GenSolutionOneToOne:
+    """
+    ========================================================================
+     Generate solution.
+    ========================================================================
+    """
+
+    @staticmethod
+    def gen_3x3() -> Solution:
+        """
+        ========================================================================
+         Generate a 3x3 solution.
+        ========================================================================
+        """
+        state = GenStateOneToOne.gen_3x3()
+        stats = GenStatsPath.gen_10x20()
+        cache = GenCache.gen_3x3()
+        return Solution(is_valid=True, cache=cache, state=state, stats=stats)
