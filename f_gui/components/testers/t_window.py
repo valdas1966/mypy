fae7c11406097d8cebc88c5a990903c7fbf8ba91
valdas1966 <valdas.ivanauskas.1966@gmail.com>
2025-06-15 10:49:28 +0300

    h

---
+++ f_gui/components/testers/t_window.py
@@ -0,0 +1,16 @@
+from f_gui.components.window import Window
+from f_gui.layout.generators.g_rect import GenRect
+
+
+def test_window() -> None:
+    """
+    ========================================================================
+     Test the Window.
+    ========================================================================
+    """
+    win = Window()
+    assert win.name == 'Window'
+    assert win.parent is None
+    assert win.children == dict()
+    rect_full = GenRect.full()
+    assert win.bounds.absolute == rect_full
---
+++ f_gui/components/window.py
@@ -0,0 +1,19 @@
+from f_gui.components.component import Component
+
+
+class Window(Component):
+    """
+    ========================================================================
+     A UI component that can contain children and manage layout.
+    ========================================================================
+    """
+
+    def __init__(self,
+                 key: str = 'Window'  # Unique id for the Window
+                 ) -> None:
+        """
+        ========================================================================
+         Initialize the Window.
+        ========================================================================
+        """
+        Component.__init__(self, name=key)
