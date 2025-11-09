9ec75bdd2bc6d5b39fec4d592bef4c5cb15ab2a3
valdas1966 <valdas.ivanauskas.1966@gmail.com>
2025-11-09 17:44:01 +0200

    h

---
+++ f_search/algos/i_1_oospp/i_1_astar/_tester.py
@@ -0,0 +1,10 @@
+from f_search.algos.i_1_oospp.i_1_astar import AStar, SolutionOOSPP
+
+
+def test_without_obstacles() -> None:
+    """
+    ========================================================================
+     Test AStar algorithm without obstacles.
+    ========================================================================
+    """
+    solutio
