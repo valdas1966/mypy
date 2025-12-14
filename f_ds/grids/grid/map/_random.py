295df116703b4beef655b6e006a85870ecfa4136
Valdas <valdas.ivanauskas.1966@gmail.com>
2025-12-14 15:45:24 +0200

    h

---
+++ f_ds/grids/grid/map/_random.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+from f_utils.iter.u_iter import pairs
+
+if TYPE_CHECKING:
+    from f_ds.grids.grid.map.main import GridMap as Grid, CellMap as Cell
+
+
+class Random:
+    """
+    ============================================================================
+     Random Class for GridMap.
+    ============================================================================
+    """
+
+    def __init__(self, grid: Grid) -> None:
+        """
+        ========================================================================
+         Initialize the Random object.
+        ========================================================================
+        """
+        self._grid = grid
+
+    def pairs(self,
+              size: int,
+              min_distance: int) -> list[tuple[Cell, Cell]]:
+        """
+        ========================================================================
+         Return a list of pairs of cells from the grid.
+        ========================================================================
+        """
+        predicate = lambda x, y: x.distance(other=y) >= min_distance
+        retries = size * 100
+        return pairs(data=self._grid, size=size, predicate=predicate, retries=retries)
