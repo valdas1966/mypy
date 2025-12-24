02bc4f89fa571298b85b6a9fa549c5bfb552537b
Valdas <valdas.ivanauskas.1966@gmail.com>
2025-12-24 18:42:50 +0200

    h

--- f_ds/grids/grid/base/_select.py
+++ f_ds/grids/grid/base/_select.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
+from f_math.shapes import Rect
 from f_utils import u_iter
 from typing import TYPE_CHECKING, Callable
 
 if TYPE_CHECKING:
-    from f_ds.grids.grid.base import GridBase as Grid, CellBase as Cell
+    from f_ds.grids.grid import GridBase as Grid
+    from f_ds.grids.cell import CellBase as Cell
 
 
 class Select:
@@ -41,6 +43,7 @@ class Select:
     def random_in_range(self,
                         size: int = None,
                         pct: int = None,
+                        rect: Rect = None,
                         row_min: int = None,
                         col_min: int = None,
                         row_max: int = None,
@@ -50,10 +53,11 @@ class Select:
          Return a random sample of Cells from the Grid within a given range.
         ========================================================================
         """
-        row_min = row_min if row_min is not None else 0
-        col_min = col_min if col_min is not None else 0
-        row_max = row_max if row_max is not None else self._grid.rows - 1
-        col_max = col_max if col_max is not None else self._grid.cols - 1
+        if not rect:
+            row_min = row_min if row_min is not None else 0
+            col_min = col_min if col_min is not None else 0
+            row_max = row_max if row_max is not None else self._grid.rows - 1
+            col_max = col_max if col_max is not None else self._grid.cols - 1
 
         def predicate(cell: Cell) -> bool:
             """
@@ -61,11 +65,8 @@ class Select:
              Predicate to filter Cells within a given range.
             ========================================================================
             """
-            is_valid_row = row_min <= cell.row <= row_max
-            is_valid_col = col_min <= cell.col <= col_max
-            return is_valid_row and is_valid_col
+            return cell.is_within(rect, row_min, row_max, col_min, col_max)
+
 
         cells_within = u_iter.filter(items=self._grid, predicate=predicate)
         return u_iter.sample(items=cells_within, size=size, pct=pct)
-
-   
